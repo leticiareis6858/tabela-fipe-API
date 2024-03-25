df923847be38cd148235cdf6399446d4c5a9c5bf
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-25 18:43:23 -0300

    feat: permite a busca do modelo  por nome ou trecho do nome

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -2,20 +2,18 @@ package br.com.alura.TabelaFipe.principal;
 
 import br.com.alura.TabelaFipe.model.Dados;
 import br.com.alura.TabelaFipe.model.ModelosVeiculos;
+import br.com.alura.TabelaFipe.model.Veiculo;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
 import br.com.alura.TabelaFipe.service.ConverteDados;
 
-import java.util.Comparator;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner scanner = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
-    private ConverteDados conversor=new ConverteDados();
+    private ConverteDados conversor = new ConverteDados();
 
     public void exibeMenu() {
         int opcao = 0;
@@ -58,28 +56,28 @@ public class Principal {
                 String json = consumoApi.obterDados(endereco);
                 System.out.println(json);
 
-                var marcas=conversor.obterLista(json, Dados.class);
+                var marcas = conversor.obterLista(json, Dados.class);
                 marcas.stream()
                         .sorted(Comparator.comparing(Dados::nome))
                         .forEach(System.out::println);
 
                 System.out.println("Digite o código da marca a ser consultada: ");
-                var codigoMarca=scanner.nextLine();
+                var codigoMarca = scanner.nextLine();
 
-                endereco=endereco+"/"+codigoMarca+"/modelos";
-                json=consumoApi.obterDados(endereco);
-                var modeloLista=conversor.obterDados(json, ModelosVeiculos.class);
+                endereco = endereco + "/" + codigoMarca + "/modelos";
+                json = consumoApi.obterDados(endereco);
+                var modeloLista = conversor.obterDados(json, ModelosVeiculos.class);
 
                 System.out.println("Modelos dessa marca:\n");
                 modeloLista.modelosVeiculos().stream()
                         .sorted(Comparator.comparing(Dados::nome))
                         .forEach(System.out::println);
 
-                System.out.println("Digite o código do modelo a ser consultado: ");
-                var codigoVeiculo=scanner.nextLine();
+                System.out.println("Digite o nome ou um trecho do nome do modelo a ser consultado: ");
+                var nomeVeiculo = scanner.nextLine();
 
-                List<Dados> modelosFiltrados=modeloLista.modelosVeiculos().stream()
-                        .filter(m->m.codigo().equals(codigoVeiculo))
+                List<Dados> modelosFiltrados = modeloLista.modelosVeiculos().stream()
+                        .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
                         .collect(Collectors.toList());
 
                 System.out.println("\nModelos do veículo filtrados: ");
