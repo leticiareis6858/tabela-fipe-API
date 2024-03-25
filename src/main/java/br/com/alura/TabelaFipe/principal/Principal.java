8860438015901530cda35b120af11d1c12814707
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2024-03-25 17:37:09 -0300

    feat: adiciona busca por modelos de veÃículos a partir da marca

--- src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/principal/Principal.java
@@ -1,7 +1,11 @@
 package br.com.alura.TabelaFipe.principal;
 
+import br.com.alura.TabelaFipe.model.Dados;
+import br.com.alura.TabelaFipe.model.ModelosVeiculos;
 import br.com.alura.TabelaFipe.service.ConsumoApi;
+import br.com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,6 +13,7 @@ public class Principal {
     private Scanner scanner = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+    private ConverteDados conversor=new ConverteDados();
 
     public void exibeMenu() {
         int opcao = 0;
@@ -50,6 +55,23 @@ public class Principal {
             if (endereco != null) {
                 String json = consumoApi.obterDados(endereco);
                 System.out.println(json);
+
+                var marcas=conversor.obterLista(json, Dados.class);
+                marcas.stream()
+                        .sorted(Comparator.comparing(Dados::nome))
+                        .forEach(System.out::println);
+
+                System.out.println("Digite o código da marca a ser consultada: ");
+                var codigoMarca=scanner.nextLine();
+
+                endereco=endereco+"/"+codigoMarca+"/modelos";
+                json=consumoApi.obterDados(endereco);
+                var modeloLista=conversor.obterDados(json, ModelosVeiculos.class);
+
+                System.out.println("Modelos dessa marca:\n");
+                modeloLista.modelosVeiculos().stream()
+                        .sorted(Comparator.comparing(Dados::nome))
+                        .forEach(System.out::println);
             }
         } catch (InputMismatchException e) {
             System.out.println("Entrada inválida. Por favor, insira um número.");
